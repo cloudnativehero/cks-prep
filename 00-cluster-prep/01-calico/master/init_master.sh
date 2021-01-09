611db6b83b472ce4b14a0c30cbcaf8514d0ceaae
Swapnil Kulkarni <coolsvap@gmail.com>
2021-01-09 21:35:43 +0530

    Added calico

--- 00-cluster-prep/01-calico/master/init_master.sh
+++ 00-cluster-prep/01-calico/master/init_master.sh
@@ -4,12 +4,14 @@
 KUBE_VERSION=1.19.6
 HOST_IP=`/sbin/ifconfig enp0s8 | egrep -o 'inet [0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}'  | cut -d' ' -f2`
 ### init k8s
-kubeadm init --apiserver-advertise-address=${HOST_IP} --kubernetes-version=${KUBE_VERSION} --ignore-preflight-errors=NumCPU --skip-token-print
+kubeadm init --apiserver-advertise-address=${HOST_IP} --kubernetes-version=${KUBE_VERSION} --pod-network-cidr=192.168.0.0/16 --ignore-preflight-errors=NumCPU --skip-token-print
 ip route add 10.96.0.0/16 dev enp0s8 src ${HOST_IP}
 
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+kubectl taint nodes --all node-role.kubernetes.io/master-
 
 kubectl create -f https://docs.projectcalico.org/manifests/tigera-operator.yaml
 kubectl create -f https://docs.projectcalico.org/manifests/custom-resources.yaml
--- 00-cluster-prep/01-calico/worker/destroy_worker.sh
+++ 00-cluster-prep/01-calico/worker/destroy_worker.sh
@@ -2,5 +2,5 @@
 # Use this script to destroy worker node prior to shut down
 
 kubeadm reset -f
-rm -rf /etc/cni/net.d /etc/kubernetes /var/lib/etcd /var/lib/kubelet /var/run/kubernetes /var/lib/cni
+rm -rf ~/.kube /etc/cni/net.d /etc/kubernetes /var/lib/etcd /var/lib/kubelet /var/run/kubernetes /var/lib/cni
 iptables -F
