c2b9ef8e0e970ac09bd7c60c99ad60d2e477c737
coolsvap <coolsvap@gmail.com>
2021-01-09 20:32:07 +0530

    Added calico

--- 00-cluster-prep/01-calico/master/destroy_master.sh
+++ 00-cluster-prep/01-calico/master/destroy_master.sh
@@ -2,5 +2,5 @@
 # Use this script to destroy master prior to shut down
 
 kubeadm reset -f
-rm -rf /etc/cni/net.d /etc/kubernetes /var/lib/etcd /var/lib/kubelet /var/run/kubernetes /var/lib/cni ~/.kube
+rm -rf /etc/cni/net.d /etc/kubernetes /var/lib/etcd /var/lib/kubelet /var/run/kubernetes /var/lib/cni
 iptables -F
--- 00-cluster-prep/01-calico/master/init_master.sh
+++ 00-cluster-prep/01-calico/master/init_master.sh
@@ -4,7 +4,7 @@
 KUBE_VERSION=1.19.6
 HOST_IP=`/sbin/ifconfig enp0s8 | egrep -o 'inet [0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}'  | cut -d' ' -f2`
 ### init k8s
-kubeadm init --apiserver-advertise-address=${HOST_IP} --kubernetes-version=${KUBE_VERSION} --ignore-preflight-errors=NumCPU --pod-network-cidr=192.168.0.0/16 --skip-token-print
+kubeadm init --apiserver-advertise-address=${HOST_IP} --kubernetes-version=${KUBE_VERSION} --ignore-preflight-errors=NumCPU --skip-token-print
 ip route add 10.96.0.0/16 dev enp0s8 src ${HOST_IP}
 
 mkdir -p $HOME/.kube
