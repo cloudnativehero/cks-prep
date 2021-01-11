77a7cc7b1ae3f56a85a5b9b8c6dd712bdddd256d
Swapnil Kulkarni <coolsvap@gmail.com>
2021-01-11 13:49:10 +0530

    updated upgrade scenario

--- 19-upgrade-scenario/upgrade/1.19.6/upgrade_worker.sh
+++ 19-upgrade-scenario/upgrade/1.19.6/upgrade_worker.sh
@@ -3,12 +3,16 @@
 kubectl drain cks-worker --ignore-daemonsets
 kubectl get nodes
 
-apt-get install kubeadm=1.19.6-00 kubelet=1.19.6-00 kubectl=1.19.6-00
-kubeadm upgrade plan
-kubeadm upgrade apply 1.19.6
-kubectl get nodes
-kubectl uncordon node cks-worker
+ssh root@cks-worker <<SCRIPT
+
+apt-get install kubeadm=1.19.6-00 kubelet=1.19.6-00
+kubeadm upgrade node
 
 systemctl daemon-reload
 systemctl restart kubelet
+
+SCRIPT
+
+kubectl uncordon cks-worker
+kubectl get nodes
 
