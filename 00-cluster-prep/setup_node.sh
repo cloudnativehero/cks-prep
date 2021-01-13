02343f9b2e9aebd5e9d94a6146aeb6c0db5ef12a
Swapnil Kulkarni <coolsvap@gmail.com>
2021-01-13 19:12:30 +0530

    Updated node setup instructions

--- 00-cluster-prep/setup_node.sh
+++ 00-cluster-prep/setup_node.sh
@@ -5,7 +5,7 @@
 
 ### setup terminal
 KUBE_VERSION=1.19.6
-apt-get install -y bash-completion binutils apparmor-utils
+### Setting up background to operate Kubernetes
 echo 'colorscheme ron' >> ~/.vimrc
 echo 'set tabstop=2' >> ~/.vimrc
 echo 'set shiftwidth=2' >> ~/.vimrc
@@ -16,39 +16,41 @@ echo 'alias c=clear' >> ~/.bashrc
 echo 'complete -F __start_kubectl k' >> ~/.bashrc
 sed -i '1s/^/force_color_prompt=yes\n/' ~/.bashrc
 
+apt-get update 
+apt-get install wget apt-transport-https gnupg lsb-release -y
+
+curl -s https://falco.org/repo/falcosecurity-3672BA8F.asc | apt-key add -
+echo "deb https://dl.bintray.com/falcosecurity/deb stable main" > /etc/apt/sources.list.d/falcosecurity.list
+
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+cat <<EOF > /etc/apt/sources.list.d/kubernetes.list
+deb http://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+
+wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | sudo apt-key add -
+echo "deb https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main" > /etc/apt/sources.list.d/trivy.list
 
 ### install k8s and docker
 apt-get remove -y docker.io kubelet kubeadm kubectl kubernetes-cni docker-ce
 apt-get autoremove -y
-apt-get install -y etcd-client vim build-essential
+systemctl daemon-reload
+
+apt-get update -y
+apt-get -y install linux-headers-$(uname -r)
+apt-get install -y etcd-client vim build-essential bash-completion binutils apparmor-utils falco docker.io kubelet=${KUBE_VERSION}-00 kubeadm=${KUBE_VERSION}-00 kubectl=${KUBE_VERSION}-00 kubernetes-cni=0.8.7-00 trivy
 
 ### install kube-bench
 curl -L https://github.com/aquasecurity/kube-bench/releases/download/v0.3.1/kube-bench_0.3.1_linux_amd64.deb -o /tmp/kube-bench_0.3.1_linux_amd64.deb
 sudo apt install /tmp/kube-bench_0.3.1_linux_amd64.deb -f
 rm -rf /tmp/kube-bench*
 
-### install falco
-curl -s https://falco.org/repo/falcosecurity-3672BA8F.asc | apt-key add -
-echo "deb https://dl.bintray.com/falcosecurity/deb stable main" | tee -a /etc/apt/sources.list.d/falcosecurity.list
 
 ### install kubesec
-
 curl -L https://github.com/controlplaneio/kubesec/releases/download/v2.8.0/kubesec_linux_386.tar.gz -o /tmp/kubesec_linux_386.tar.gz
+mkdir -p /tmp/kubesec
 tar -xvf /tmp/kubesec_linux_386.tar.gz -C /tmp/kubesec/
 cp /tmp/kubesec/kubesec /usr/bin/kubesec
 rm -rf /tmp/kubesec*
-
-apt-get update -y
-apt-get -y install linux-headers-$(uname -r)
-apt-get install -y falco
-
-systemctl daemon-reload
-curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-cat <<EOF > /etc/apt/sources.list.d/kubernetes.list
-deb http://apt.kubernetes.io/ kubernetes-xenial main
-EOF
-apt-get update
-apt-get install -y docker.io kubelet=${KUBE_VERSION}-00 kubeadm=${KUBE_VERSION}-00 kubectl=${KUBE_VERSION}-00 kubernetes-cni=0.8.7-00
 
 cat > /etc/docker/daemon.json <<EOF
 {
@@ -73,3 +75,4 @@ systemctl enable kubelet && systemctl start kubelet
 
 #Pull images
 kubeadm config images pull
+
