95a7c167eb632bfa637450af7a3436b6ec7d5f34
Swapnil Kulkarni <coolsvap@gmail.com>
2021-02-16 10:24:06 +0530

    Added node refresh script

---
+++ 00-cluster-prep/refresh_node.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+# Use this script to setup any node in your Kuberntes cluster 
+# Either master or worker
+# Source: http://kubernetes.io/docs/getting-started-guides/kubeadm/
+
+### setup terminal
+KUBE_VERSION=1.19.7
+
+### install k8s and docker
+apt-get remove -y kubelet kubernetes-cni
+apt-get autoremove -y
+systemctl daemon-reload
+
+apt-get update
+apt-get -y install linux-headers-$(uname -r)
+apt-get install -y etcd-client vim build-essential bash-completion binutils apparmor-utils falco kubelet=${KUBE_VERSION}-00 kubeadm=${KUBE_VERSION}-00 kubectl=${KUBE_VERSION}-00 kubernetes-cni=0.8.7-00 trivy
+
+cat > /etc/docker/daemon.json <<EOF
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "storage-driver": "overlay2"
+}
+EOF
+mkdir -p /etc/systemd/system/docker.service.d
+
+# Restart docker.
+systemctl daemon-reload
+systemctl restart docker
+
+# start docker on reboot
+systemctl enable docker
+
+#stop and disable falco
+systemctl disable falco
+systemctl stop falco
+
+docker info | grep -i "storage"
+docker info | grep -i "cgroup"
+
+systemctl enable kubelet && systemctl start kubelet
+
